123c761e7c7de7e22c4ae994cc7df55f5800d7a6
Shashank <s_shashank@outlook.com>
2023-04-14 23:21:27 +0530

    feat: Get all users for admin only

---
+++ backend/controllers/user/getAllUserAccountController.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler';
+import User from '../../models/UserModel.js';
+
+// $-title  Get all users
+// $-path   GET /api/v1/user/all
+// $-auth   Private/Admin
+
+const getAllUserAccounts = asyncHandler(async (req, res) => {
+    const pageSize = 10;
+    const page = Number(req.query.pageNumber) || 1;
+    const count = await User.countDocuments({});
+    const users = await User.find()
+        .sort({ createdAt: -1 })
+        .select('-refreshToken')
+        .limit(pageSize)
+        .skip(pageSize * (page - 1))
+        .lean();
+
+    res.status(200).json({
+        success: true,
+        count,
+        numberOfPages: Math.ceil(count / pageSize),
+        users,
+    });
+});
+
+export default getAllUserAccounts;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,8 @@ import getUserProfile from '../controllers/user/getUserProfileController.js';
 import checkAuth from '../middleware/checkAuthMiddleware.js';
 import updateUserProfile from '../controllers/user/updateUserProfileController.js';
 import deleteUserProfile from '../controllers/user/deleteAccountController.js';
+import getAllUserAccounts from '../controllers/user/getAllUserAccountController.js';
+import role from '../middleware/roleMiddleware.js';
 
 const router = express.Router();
 
@@ -11,5 +13,9 @@ router
     .get(checkAuth, getUserProfile)
     .patch(checkAuth, updateUserProfile)
     .delete(checkAuth, deleteUserProfile);
+
+router
+    .route('/all')
+    .get(checkAuth, role.checkRole(role.ROLES.Admin), getAllUserAccounts);
 
 export default router;
